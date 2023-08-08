084fbe64639a8a42c25382c3f575ad5e499d160f
Daniil Bezuglov <xlaystgoku@gmail.com>
2023-08-08 10:33:36 +0700

    feat: added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,23 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 import Layout from '@/components/layout'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-PJ6YMGQWSR" />
+      <Script id="google-analytics">
+        {
+          `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-PJ6YMGQWSR');
+          `
+        }
+      </Script>
       <Layout>
         <Component {...pageProps} />
       </Layout>
